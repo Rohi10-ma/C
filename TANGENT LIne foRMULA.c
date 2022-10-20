c8a42b783c04d7dae6ccc198082ceaa77b252393
Rohi10-ma <113897212+Rohi10-ma@users.noreply.github.com>
2022-10-20 15:08:58 +0500

    Add files via upload

---
+++ TANGENT LIne foRMULA.c
@@ -0,0 +1,24 @@
+//finding tangent line of a circle
+#include<stdio.h>
+float tangent_line(float x1, float y1, float x2, float y2){
+    float slope = (x2-x1)/(y2-y1);
+    return slope;
+}
+int main(){
+    float x1;
+    float x2;
+    float y1;
+    float y2;
+    printf("ENTER X1 : ");
+    scanf("%f",&x1);
+    printf("ENTER x2 : ");
+    scanf("%f",&x2);
+    printf("ENTER y1 : ");
+    scanf("%f",&y1);
+    printf("ENTER y2 : ");
+    scanf("%f",&y2);
+    float slope = tangent_line(x1,x2,y1,y2);
+    printf("SLOPE OF A CIRCLE IS %.1f\n",slope);
+    
+
+}
